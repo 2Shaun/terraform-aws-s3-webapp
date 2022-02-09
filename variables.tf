ada264fdf5f94dabeb40a7f8c67d02ff00f7c29a
Tom O'Shaughnessy <tom.w.oshaughnessy@gmail.com>
2022-02-09 07:05:33 -0500

    Add descriptions to variables to see if terraform cloud updates the registry" ;

--- variables.tf
+++ variables.tf
@@ -1,3 +1,11 @@
-variable "region" {}
-variable "prefix" {}
-variable "name" {}
+variable "region" {
+    description = "The region for the aws provider"
+}
+
+variable "prefix" {
+    description = "The prefix of the bucket name (<prefix>-<name>)"
+}
+
+variable "name" {
+    description = "The name of the bucket after the prefix (<prefix>-<name>)"
+}
